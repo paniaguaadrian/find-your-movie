60ec626cbb96da5b954f401037b352fea10efc7a
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-09-26 17:38:47 +0200

    Project done with freeCodeCamp

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// A simple component that renders the app header and accepts a title prop
+
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <header className="App-header">
+      <h2>{props.text}</h2>
+    </header>
+  );
+};
+
+export default Header;
